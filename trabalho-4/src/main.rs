6a3d49a7f32eeeab82e3c757b0005d26d1840d0f
Freddy Ilha <frilha@estee.com>
2025-10-26 21:58:33 -0300

    WIP: Adding statistics

--- trabalho-4/src/main.rs
+++ trabalho-4/src/main.rs
@@ -1,8 +1,14 @@
+use chrono::prelude::*;
+use csv::Writer;
 use minifb::{Key, MouseButton, Window, WindowOptions};
 use rand::Rng;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::time::{Duration, Instant};
 
-const WIDTH: usize = 400;
-const HEIGHT: usize = 400;
+const WIDTH: usize = 1000;
+const HEIGHT: usize = 1000;
 const WHITE: u32 = 0x00FFFFFF;
 const RED: u32 = 0x00FF0000;
 const BLACK: u32 = 0x00080808;
@@ -10,6 +16,42 @@ const ORANGE: u32 = 0x00FF963C;
 
 type Point = (usize, usize);
 type Polygon = Vec<Point>;
+
+struct Statistics {
+    obstacles_amount: usize,
+    points_amount: usize,
+}
+
+impl Statistics {
+    fn new() -> Self {
+        Statistics {
+            obstacles_amount: 0,
+            points_amount: 0,
+        }
+    }
+}
+
+fn save_statistics(stats: &Statistics) -> Result<(), Box<dyn Error>> {
+    let path = "stats.csv";
+    let file_exists = Path::new(path).exists();
+
+    let file = OpenOptions::new().append(true).create(true).open(path)?;
+
+    let mut wtr = Writer::from_writer(file);
+
+    if !file_exists {
+        wtr.write_record(&["timestamp", "obstacles_amount", "points_amount"])?;
+    }
+
+    wtr.write_record(&[
+        Local::now().to_string(),
+        stats.obstacles_amount.to_string(),
+        stats.points_amount.to_string(),
+    ])?;
+
+    wtr.flush()?;
+    Ok(())
+}
 
 fn draw_line(buffer: &mut [u32], x0: usize, y0: usize, x1: usize, y1: usize, color: u32) {
     let (mut x0, mut y0, x1, y1) = (x0 as i32, y0 as i32, x1 as i32, y1 as i32);
@@ -152,7 +194,7 @@ fn generate_random_obstacle(center_x: usize, center_y: usize, polygons: &mut Vec
     let mut points: Vec<Point> = Vec::new();
     for i in 0..num_vertices {
         let angle = (i as f32 / num_vertices as f32) * 2.0 * std::f32::consts::PI;
-        let radius = rng.random_range(20..=max_radius) as f32;
+        let radius = rng.random_range(10..=max_radius) as f32;
 
         let x = center_x as f32 + radius * angle.cos();
         let y = center_y as f32 + radius * angle.sin();
@@ -167,17 +209,21 @@ fn generate_random_obstacle(center_x: usize, center_y: usize, polygons: &mut Vec
 }
 
 fn main() {
+    let mut stats = Statistics::new();
     let mut polygons: Vec<Polygon> = Vec::new();
     let mut polygons_expanded: Vec<Polygon> = Vec::new();
+    let mut last_log_time = Instant::now();
+    let robot: Polygon = vec![(200, 200), (240, 200), (240, 240), (200, 240)];
+    let mut window = Window::new("Moving Box", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut was_pressed = false;
+
     polygons.push(vec![(20, 20), (60, 20), (60, 60), (20, 60)]);
     polygons.push(vec![(200, 20), (260, 20), (260, 60), (200, 60)]);
     polygons.push(vec![(71, 272), (91, 321), (147, 314)]);
 
-    let robot: Polygon = vec![(200, 200), (240, 200), (240, 240), (200, 240)];
-
-    let mut window = Window::new("Moving Box", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    let mut was_pressed = false;
+    stats.obstacles_amount = 3;
+    stats.points_amount = 22;
 
     while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
         buffer.fill(WHITE);
@@ -208,10 +254,25 @@ fn main() {
             let mouse_y = y as usize;
 
             if is_pressed && !was_pressed {
+                let mut points_amount = 0;
+
                 generate_random_obstacle(mouse_x, mouse_y, &mut polygons);
+                stats.obstacles_amount += 1;
+
+                for polygon in &polygons {
+                    for _ in polygon {
+                        points_amount += 2;
+                    }
+                }
+
+                stats.points_amount = points_amount;
             }
         }
 
+        if last_log_time.elapsed() >= Duration::from_secs(1) {
+            save_statistics(&stats).unwrap();
+            last_log_time = Instant::now();
+        }
         was_pressed = is_pressed;
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
